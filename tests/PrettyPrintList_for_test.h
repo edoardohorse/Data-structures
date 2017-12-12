851492418599eab207de334f1898585935f72b12
Edoardo Cavallo <e.cavallo13@studenti.uniba.it>
2017-12-12 09:04:12 +0100

    Update namespaces and printListPointer

--- tests/PrettyPrintList_for_test.h
+++ tests/PrettyPrintList_for_test.h
@@ -7,6 +7,7 @@
 #include "ListArray_for_test.h"
 #include "ListCursor_for_test.h"
 #include "ListPointer_for_test.h"
+#include "ListDoublePointer_for_test.h"
 
 #define WIDTH(x) std::setw(x)
 
@@ -14,8 +15,8 @@ namespace PrintList{
 
 	// Pretty print of list
 	template <typename TypeElem, int Length>
-	void printListArray(const NListArray::ListArray<TypeElem, Length>* list, int widthBuffer = 3){
-		NListArray::Position i=list->first();
+	void printListArray(const NListArrayTest::ListArray<TypeElem, Length>* list, int widthBuffer = 3){
+		NListArrayTest::Position i=list->first();
 
 		std::cout << std::endl;
 		std::cout << (char)201 << (char)205 << WIDTH(widthBuffer);  // ╔═
@@ -36,8 +37,8 @@ namespace PrintList{
 
 	// Pretty print of list
 	template <typename TypeElem>
-	void printListCursor(const NListCursor::ListCursor<TypeElem>* list, int widthBuffer = 3) {
-		NListCursor::Position i=list->first();
+	void printListCursor(const NListCursorTest::ListCursor<TypeElem>* list, int widthBuffer = 3) {
+		NListCursorTest::Position i=list->first();
 
 
 		std::cout << std::endl;
@@ -66,8 +67,8 @@ namespace PrintList{
 	}
 
 	template<typename TypeElem>
-	void printListPointer(const NListPointer::ListPointer<TypeElem>* list, int widthBuffer = 8){
-		NListPointer::Position<TypeElem> i=list->first();
+	void printListPointer(const NListPointerTest::ListPointer<TypeElem>* list, int widthBuffer = 8){
+		NListPointerTest::Position<TypeElem> i=list->first();
 
 
 		std::string cornerLeftTop(1,(char)201);
@@ -138,6 +139,80 @@ namespace PrintList{
 		toPrint = strStream.str();
 		std::cout << toPrint;
 
+	}
+
+	template<typename TypeElem>
+	void printListDoublePointer(const NListDoublePointerTest::ListDoublePointer<TypeElem>* list, int widthBuffer = 10){
+		NListDoublePointerTest::Position<TypeElem> i=list->first();
+
+
+		std::string cornerLeftTop(1,(char)201);
+		cornerLeftTop.append(1,(char)205);
+
+		std::string cornerRightTop(1,(char)205);
+		cornerLeftTop.append(1,(char)187);
+
+		std::string cornerLeftBottom(1,(char)200);
+		cornerLeftTop.append(1,(char)205);
+
+		std::string cornerRightBottom(1,(char)205);
+		cornerLeftTop.append(1,(char)188);
+
+		std::string toPrint;
+		std::ostringstream strStream;
+		std::ostringstream::streampos posFirstLine;
+		std::ostringstream::streampos posLastLine;
+		std::ostringstream::streampos pos;
+		std::ostringstream::streampos delta;
+
+
+		posFirstLine = strStream.tellp();
+		long long int max = 0;
+
+
+
+		do{
+			pos = strStream.tellp();
+			strStream << WIDTH(2) << i;
+			strStream << (char)186;                                 // ║
+			strStream << "${|}" << list->get(i) << " ";
+			strStream << (char)124 ;                        // |
+			strStream << "${|}" << list->next(i) << " ";
+			strStream << (char)186;                                 // ║
+			strStream << std::endl;
+
+			delta = strStream.tellp();
+			delta -= sizeof(&i);
+			if(max < (delta-pos))
+				max = delta-pos;
+
+
+
+
+
+		}while(!list->isLast(i = list->next(i)));
+
+		posLastLine = strStream.tellp();
+
+		strStream.seekp(posFirstLine);
+		strStream << std::string((size_t)max, (char)205) << std::endl << strStream.str();
+
+		strStream.seekp(posLastLine+max+1);
+		strStream << std::string((size_t)max, (char) 205);
+
+		toPrint = strStream.str() + "\n";
+		strStream.seekp(0);
+		std::size_t last = toPrint.rfind("${|}");
+		std::size_t found = 0;
+		while ( found != last ){
+			found = toPrint.find("${|}" ,found+1);
+			strStream.seekp(found);
+			strStream << "c" << strStream.str();
+
+			//toPrint.replace(found,4, std::setw));
+		}
+		toPrint = strStream.str();
+		std::cout << toPrint;
 	}
 }
 
