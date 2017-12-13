e015f416d1e3d80e41c47cd02539157627836800
Edoardo Cavallo <e.cavallo13@studenti.uniba.it>
2017-12-13 11:11:17 +0100

    Fixed isLast method

--- ListCircular.h
+++ ListCircular.h
@@ -87,7 +87,7 @@ namespace NListCircular{
 
 	template<typename TypeElem>
 	bool ListCircular<TypeElem>::isLast(Position<TypeElem> p) const{
-		return (p == sentinel->getPrevious());
+		return (p == sentinel);
 	}
 
 	template<typename TypeElem>
